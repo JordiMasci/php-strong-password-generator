7c05e950687775b24d88bea716fe1acdd23da912
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-27 15:55:19 +0200

    feat: funzione per generare stringa casuale

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-function genera_password(){
-
+function generaStringaRandom($lunghezza)
+{
+    $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $stringaRandom = '';
+    for ($i = 0; $i < $lunghezza; $i++) {
+        $stringaRandom .= $caratteri[rand(0, strlen($caratteri) - 1)];
+    }
+    return $stringaRandom;
 }
-
-$has_password = isset($_GET['password']);
-
 ?>
 
 
