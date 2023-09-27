14f8e1f771914ee92b430e4d1016cef9f931eab3
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-27 15:49:32 +0200

    start ex

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+function genera_password(){
+
+}
+
+$has_password = isset($_GET['password']);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Inserisci password</h1>
+        <form method="GET">
+            <input type="text" name="password" id="password" required>
+            <button class="btn btn-primary">Invia</button>
+        </form>
+    </div>
+</body>
+
+</html>
